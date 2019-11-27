e377f3dd60235e34b26cdf47a5c11cbf203edb6e
user <user@localhost.localdomain>
2019-11-28 00:37:53 +0300

    lab5 backup/upback tasks done

--- lab5/backup.sh
+++ lab5/backup.sh
@@ -1,1 +1,63 @@
 #!/bin/bash
+srcdir="$HOME/source";
+if [[ ! -d $srcdir ]]; then
+	echo "Directory $srcdir not exists";
+	exit 1;
+fi;
+
+lastbackuptime=0;
+cd $HOME;
+backupslist=$(find . -maxdepth 1 -type d -printf "%f\n" | grep -soE "Backup-[0-9]{4}-[0-9]{2}-[0-9]{2}");
+if [[ -n "$backupslist"  ]]; then 
+	lastbackupdir=$(echo "$backupslist" | tail -1);
+	lastbackupdate=$(echo "$lastbackupdir" | grep -soP "Backup-\K[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+	lastbackuptime=$(date -d $lastbackupdate +%s);
+fi;
+
+currentdate=$(date +%Y-%m-%d);
+currenttime=$(date +%s);
+
+let timediff=$currenttime-$lastbackuptime;
+if [[ $timediff -gt 7*24*60*60 ]]; then
+	backupdir="$HOME/Backup-$currentdate";
+	mkdir $backupdir;
+	backupexists=false;
+else
+	
+	backupdir="$HOME/$lastbackupdir";
+fi;
+
+reportpath="$HOME/backup-report";
+cd $srcdir;
+fileslist=$(find . -maxdepth 1 -type f -printf "%f\n");
+
+if [[ $backupexists = false ]]; then
+	echo "$backupdir create  $currentdate" >> $reportpath;
+	for filename in $fileslist; do
+		cp --parents "$filename" "$backupdir/";
+		echo "$filename backup done $currentdate" >> $reportpath;
+	done;
+else
+	echo "$backupdir update  $currentdate" >> $reportpath;
+	newfilesmsgs="";
+	oldfilesmsgs="";
+	for filename in $fileslist; do
+		if [[ -e "$backupdir/$filename" ]]; then
+			oldsize=$(stat -c%s "$backupdir/$filename")
+			newsize=$(stat -c%s "$srcdir/$filename")
+
+			if [[ $oldsize != $newsize ]]; then
+				mv "$backupdir/$filename" "$backupdir/$filename.$currentdate";
+				cp "$srcdir/$filename" "$backupdir/$filename";
+
+				oldfilemsg=$oldfilemsg$(echo "$filename old backup moved to $filename.$currentdate $current_date")$'\n';
+			fi;
+		else
+			cp --parents "$filename" "$backupdir/";
+			newfilemsg=$newfilemsg$(echo "$filename backup done $currentdate")$'\n';
+		fi;
+	done;
+	echo "$newfilemsg" | sed '/^$/d' >> $reportpath;
+	echo "$oldfilemsg" | sed '/^$/d' >> $reportpath;
+fi;
+	
--- lab5/upback.sh
+++ lab5/upback.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+cd $HOME;
+backupdir=$(find . -maxdepth 1 -printf "%f\n" | sort | grep -oE "Backup-[0-9]{4}-[0-9]{2}-[0-9]{2}" | tail -1);
+if [[ -z "$backupdir" ]]; then
+	echo "Backup not found";
+	exit;
+fi;
+
+cd "$backupdir";
+files=$(find . -type f | grep -E -v ".[0-9]{4}-[0-9]{2}-[0-9]{2}");
+
+restoredir="$HOME/restore/";
+[[ ! -d "$restoredir" ]] && mkdir "$restoredir";
+
+for filename in $files; do
+	[[ $filename == ".*\.[0-9]{4}-[0-9]{2}-[0-9]{2}$" ]] && continue;
+  	cp --parents "$filename" "$restoredir/";
+done;
